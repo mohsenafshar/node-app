b9e924a27c3f34546b71d1772dda5d1d9a9043c6
mohsen afshar <mohsenafshar_ph@yahoo.com>
2019-09-12 03:52:17 +0430

    post request added

--- static-express/server.js
+++ static-express/server.js
@@ -1,22 +1,32 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {
-        name : "Mohsen",
-        message : "Hi"
+        name: "Mohsen",
+        message: "Hi"
     },
     {
-        name : "Mohammad",
-        message : "Hello"
+        name: "Mohammad",
+        message: "Hello"
     }];
 
 
-app.get("/messages", (req, res) =>{
+app.get("/messages", (req, res) => {
     messages[0].name = req.query.name;
     res.send(messages)
+})
+
+app.post("/messages", (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
+    console.log("ok");
+    
 })
 
 var server = app.listen(3000, () => {
